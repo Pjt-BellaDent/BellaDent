79c36936e8f123ffb329520911e559d5d9e459df
GOpro <rheorus@naver.com>
2025-05-15 19:06:25 +0900

    style branch 분할 준비

---
+++ packages/client/src/components/TreatmentInfo_1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TreatmentInfo_1() {
+  return (
+    <div>TreatmentInfo_1</div>
+  )
+}
+
+export default TreatmentInfo_1
---
+++ packages/client/src/components/TreatmentInfo_3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TreatmentInfo_3() {
+  return (
+    <div>TreatmentInfo_3</div>
+  )
+}
+
+export default TreatmentInfo_3
