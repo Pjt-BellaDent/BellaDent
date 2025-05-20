9d1d2c0b841d6852c1e95c1b7e155f735ffa418f
GOpro <rheorus@naver.com>
2025-05-20 20:03:27 +0900

    Feat: signIn 함수 작성
    signIn 기능 구현
    client
    form 입력 내용 axios를 통해 post로 server로 전송
    server
    전송받은 email 기준으로 비밀번호 일치 확인

--- packages/client/src/components/SignIn.jsx
+++ packages/client/src/components/SignIn.jsx
@@ -1,43 +1,64 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 function SignIn() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = 'http://localhost:3000/users/signIn';
+      const response = await axios.post(url, {
+        email: formData.email,
+        password: formData.password,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="signin-form space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
-              for="email"
+              htmlFor="email"
               className="block text-sm/6 font-medium text-gray-900"
             >
-              Email address
+              이메일
             </label>
             <div className="mt-2">
               <input
                 type="email"
                 name="email"
                 id="email"
-                autocomplete="email"
+                onChange={handleChange}
                 required
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
           </div>
-
           <div>
             <div className="flex items-center justify-between">
               <label
-                for="password"
+                htmlFor="password"
                 className="block text-sm/6 font-medium text-gray-900"
               >
-                Password
+                비밀번호
               </label>
               <div className="text-sm">
                 <a
                   href="#"
                   className="font-semibold text-indigo-600 hover:text-indigo-500"
                 >
-                  Forgot password?
+                  비밀번호를 잊어버리셨나요?
                 </a>
               </div>
             </div>
@@ -46,23 +67,21 @@ function SignIn() {
                 type="password"
                 name="password"
                 id="password"
-                autocomplete="current-password"
+                onChange={handleChange}
                 required
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
           </div>
-
           <div>
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              로그인
             </button>
           </div>
         </form>
-
         <p className="mt-10 text-center text-sm/6 text-gray-500">
           Not a member?
           <a
