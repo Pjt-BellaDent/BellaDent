b59dc0696d50bfdbe1d374278b4da003a7c2d5fe
sunghoon711 <sunghoon824711@gamil.com>
2025-05-21 17:51:30 +0900

    1. dashboard ui 수정 -오늘 진료 / 예약 건수 / 대기 환자 클릭시 생성되는 폼 수정 -읍급 건수 삭제 -그래프 및 내부 구조 위치 변경
    2. 로그인시 직원 권한별로 crm 볼 수 있게 수정중
    -sidebar에서 메뉴별로 권한에 따라 볼 수 있게끔 처리함.
    -role 기능으로 구현/테스트
    -dashboard.jsx에서 로그인 / 권한 테스트

--- packages/client/src/routes/Frame.jsx
+++ packages/client/src/routes/Frame.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Outlet } from 'react-router';
+import { Outlet } from 'react-router-dom';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 
-function Frame() {
-  const Wrapper = styled.div`
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-  `;
-  const Mein = styled.main`
-    flex-grow: 1;
-  `;
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
+const Main = styled.main`
+  flex-grow: 1;
+`;
+
+function Frame() {
   return (
     <Wrapper>
       <Header />
-      <Mein>
+      <Main>
         <Outlet />
-      </Mein>
+      </Main>
       <Footer />
     </Wrapper>
   );
-};
+}
 
 export default Frame;
