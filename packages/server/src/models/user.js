e8ed0dbb93828dde9b9445e48850b6ba7eca942e
GOpro <rheorus@naver.com>
2025-05-23 12:02:03 +0900

    Feat: 스키마 항목 추가

--- packages/server/src/models/user.js
+++ packages/server/src/models/user.js
@@ -10,6 +10,8 @@ export const userSchema = Joi.object({
     .pattern(/^01[016789]-\d{3,4}-\d{4}$/)
     .required(),
   address: Joi.string().required(),
+  birthDate: Joi.date().allow(null).optional(),
+  gender: Joi.string().valid("남성", "여성").allow(null).optional(),
   role: Joi.string().valid("patient", "staff", "admin").default("patient"),
   createdAt: Joi.date().required(),
   updatedAt: Joi.date().default(() => new Date()),
