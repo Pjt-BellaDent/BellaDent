4cccbbfc4d9686ede0623e11a3cfd6fcd35badbb
choi <ms10111@nate.com>
2025-05-23 14:46:38 +0900

    Design size 맞추어  Export 하였으며 밑줄구성 수정하여 작업 완성을 하였습니다

--- packages/client/src/components/TreatmentInfo_1_3.jsx
+++ packages/client/src/components/TreatmentInfo_1_3.jsx
@@ -12,10 +12,10 @@ function TreatmentInfo_1_3() {
   return (
     <div>
     
-    <header>하얀치아 예쁜미소</header>
+    <header className='mt-3'><b>하얀치아 예쁜미소</b></header>
    
      
-      <p className='text-center'>Lorem ipsum dolor sit amet.</p>
+      <p className='text-center mt-6 mb-3'>하얀 치아 교정</p>
          <div className="review d-flex gap-2">
            <img src={ch1} alt="ch1" />;
            <img src={ch} alt="ch"  />;
@@ -23,9 +23,9 @@ function TreatmentInfo_1_3() {
            <img src={siml} alt="siml" />;
          </div>
         
-         <p className='text-center'>Lorem ipsum dolor sit amet.</p>
+         <p className='text-center mt-6 mb-3'>하얀 치아 미소</p>
        
-         <div className="review d-flex gap-2">
+         <div className="review d-flex gap-2 mb-10">
            <img src={sm} alt="sm" />;
            <img src={smi} alt="smi" />;
            <img src={laugh} alt="laugh" />;
