baf15d38511a9708b9bdd721fc1b19607dbd9dea
GOpro <rheorus@naver.com>
2025-05-28 14:41:48 +0900

    Feat: firebase storage 연결
    firebase storage 연결 초기화

--- packages/server/src/config/firebase.js
+++ packages/server/src/config/firebase.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { readFile } from "fs/promises";
 import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from 'firebase-admin/storage';
 import { getAuth } from "firebase-admin/auth";
 
 dotenv.config();
@@ -11,7 +12,9 @@ const serviceAccount = JSON.parse(
 );
 initializeApp({
   credential: cert(serviceAccount),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
 });
 
 export const db = getFirestore();
+export const bucket = getStorage().bucket();
 export const auth = getAuth();
