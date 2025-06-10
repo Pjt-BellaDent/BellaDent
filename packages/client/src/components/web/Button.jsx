5de57129c60f14be51f39e2eadab14536e745218
GOpro <rheorus@naver.com>
2025-06-10 12:16:35 +0900

    Style: 불필요 코드 삭제

--- packages/client/src/components/web/Button.jsx
+++ packages/client/src/components/web/Button.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Button({ children, CN, CLICK }) {
   return (
-    <div>
+    <>
       <button type="button" className={CN} onClick={CLICK}>
         {children}
       </button>
-    </div>
+    </>
   );
 }
 
