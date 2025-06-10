2f7b30c55ffba5580c6e591f14b8c90ff4252beb
GOpro <rheorus@naver.com>
2025-06-10 12:15:56 +0900

    Feat: 로그인 인증 api 변경

--- packages/client/src/routes/web/users/UserInfo.jsx
+++ packages/client/src/routes/web/users/UserInfo.jsx
@@ -1,20 +1,23 @@
 import react from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useUserInfo } from '../../../contexts/UserInfoContext.jsx';
-import Cookies from 'js-cookie';
 import axios from 'axios';
 
 function UserInfo() {
   const navigate = useNavigate();
-  const { userInfo, isLogin, setIsLogin } = useUserInfo;
+  const { userInfo, userToken, isLogin, setIsLogin } = useUserInfo;
 
-  const handleDelete = async () => {
+  const handleDisabled = async () => {
     try {
       confirm('정말로 회원탈퇴를 진행 하시겠습니까?');
-      const url = `http://localhost:3000/users/${userInfo.id}`;
-      const response = await axios.delete(url);
+      const url = `http://localhost:3000/users/disabled/${userInfo.id}`;
+      const response = await axios.put(url, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+        withCredentials: true,
+      });
       if (response.status === 201) {
-        Cookies.remove('token');
         alert('회원탈퇴가 완료되었습니다.');
         setIsLogin(!isLogin);
         navigate(0);
@@ -29,7 +32,7 @@ function UserInfo() {
       <button className="mx-auto w-full">
         <Link to={'/user-update'}>회원정보수정</Link>
       </button>
-      <button className="mx-auto w-full cursor-pointer" onClick={handleDelete}>
+      <button className="mx-auto w-full cursor-pointer" onClick={handleDisabled}>
         회원탈퇴
       </button>
     </>
