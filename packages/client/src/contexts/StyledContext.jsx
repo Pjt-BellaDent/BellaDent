9bca90e1df96b54be7676371daf185aa5cea2810
GOpro <rheorus@naver.com>
2025-06-17 12:56:21 +0900

    Refactor: context 기능 개선

---
+++ packages/client/src/contexts/StyledContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useContext } from 'react';
+
+const StyledContext = createContext(null);
+
+export const styledProvider = ({ children }) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  return (
+    <styledProvider.Provider
+      value={{
+        scrolled,
+        setScrolled,
+      }}
+    >
+      {children}
+    </styledProvider.Provider>
+  );
+};
+
+export const useStyled = () => {
+  return useContext(StyledContext);
+};
