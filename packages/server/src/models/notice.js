78bad056c7786ee2fb61a33b7030c7f88b3ab533
GOpro <rheorus@naver.com>
2025-06-18 11:33:32 +0900

    Feat: key 항목 추가

--- packages/server/src/models/notice.js
+++ packages/server/src/models/notice.js
@@ -4,6 +4,7 @@ export const noticeSchema = Joi.object({
   title: Joi.string().required(),
   content: Joi.string().required(),
   authorId: Joi.string().required(),
+  accessType: Joi.string().valid("internal", "external").default("internal"),
   isPublic: Joi.boolean().default(true),
 });
 
@@ -11,5 +12,6 @@ export const updateNoticeSchema = Joi.object({
   title: Joi.string().optional(),
   content: Joi.string().optional(),
   authorId: Joi.string().optional(),
+  accessType: Joi.string().valid("internal", "external").default("internal"),
   isPublic: Joi.boolean().optional(),
 });
