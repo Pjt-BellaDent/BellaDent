a9098fa150b7b8f8bacc5a8fba43edc4021b6865
GOpro <rheorus@naver.com>
2025-06-18 09:26:22 +0900

    Design: 디자인 통일

--- packages/client/src/components/web/Modal.jsx
+++ packages/client/src/components/web/Modal.jsx
@@ -16,11 +16,11 @@ const Modal = ({ children, show, setShow, activeClick, activeClose }) => {
       className="fixed top-0 left-0 w-full h-full items-center justify-center z-50 bg-neutral-950/60"
       style={{ display: show ? 'flex' : 'none' }}
     >
-      <div className="bg-white p-5 rounded-lg w-150 max-h-[80vh] overflow-y-auto ">
+      <div className="bg-white px-20 py-15 rounded-lg max-w-200 min-w-120 max-h-[80vh] overflow-y-auto text-xl ">
         {children}
-        <div className="mt-2.5 flex justify-end">
+        <div className="mt-20 flex justify-end">
           <button
-            className="bg-green-600 px-3 py-1.5 rounded-md text-white cursor-pointer"
+            className="flex justify-center rounded-md bg-BD-CharcoalBlack text-BD-ElegantGold outline-2 -outline-offset-2 outline-BD-CharcoalBlack px-6 py-3 shadow-xs hover:bg-BD-ElegantGold  hover-visible:outline-BD-ElegantGold hover:text-BD-CharcoalBlack focus:bg-BD-ElegantGold  focus-visible:outline-BD-ElegantGold focus:text-BD-CharcoalBlack duration-300"
             onClick={handleClick}
           >
             확인
