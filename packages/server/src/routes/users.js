9e291eceba3087947364a73ad85a2cc97ab9c7dd
GOpro <rheorus@naver.com>
2025-06-19 09:11:29 +0900

    Refactor: 코드 간소화

--- packages/server/src/routes/users.js
+++ packages/server/src/routes/users.js
@@ -30,29 +30,29 @@ import {
 } from "../middleware/roleCheck.js";
 
 const router = express.Router();
-const auth = authenticateFirebaseToken; // Firebase 인증 미들웨어
+const auth = authenticateFirebaseToken;
 
 router.post("/signUp", signUp); // 홈페이지 회원가입
-router.post("/patient", authenticateFirebaseToken, staffRoleCheck, CreatePatient); // 환자 등록
-router.post("/staff", authenticateFirebaseToken, managerRoleCheck, CreateStaff); // 직원 등록
+router.post("/patient", auth, staffRoleCheck, CreatePatient); // 환자 등록
+router.post("/staff", auth, managerRoleCheck, CreateStaff); // 직원 등록
 
-router.post("/signIn", authenticateFirebaseToken, signIn); // 로그인
+router.post("/signIn", auth, signIn); // 로그인
 
 // 직원 전체 목록 조회 (항상 :id 라우트보다 위에 위치해야 함)
 router.get("/staff", getAllStaff);
 
-router.get("/:id", authenticateFirebaseToken, patientRoleCheck, getUserById); // 홈페이지 회원 정보 조회
-router.get("/patient", authenticateFirebaseToken, staffRoleCheck, getPatients); // 전체 환자 상세 정보 조회
-router.get("/patient/:id", authenticateFirebaseToken, staffRoleCheck, getPatientById); // 환자 상세 정보 조회 (id)
-router.get("/patient/name/:id", authenticateFirebaseToken, staffRoleCheck, getPatientByName); // 환자 상세 정보 조회 (name)
-router.get("/staff/:id", authenticateFirebaseToken, staffRoleCheck, getStaffById); // 직원 상세 정보 조회
+router.get("/:id", auth, patientRoleCheck, getUserById); // 홈페이지 회원 정보 조회
+router.get("/patient", auth, staffRoleCheck, getPatients); // 전체 환자 상세 정보 조회
+router.get("/patient/:id", auth, staffRoleCheck, getPatientById); // 환자 상세 정보 조회 (id)
+router.get("/patient/name/:id", auth, staffRoleCheck, getPatientByName); // 환자 상세 정보 조회 (name)
+router.get("/staff/:id", auth, staffRoleCheck, getStaffById); // 직원 상세 정보 조회
 
-router.put("/:id", authenticateFirebaseToken, patientRoleCheck, updateUser); // 홈페이지 회원 정보 수정
-router.put("/patient/:id", authenticateFirebaseToken, staffRoleCheck, updatePatient); // 환자 정보 수정
-router.put("/staff/:id", authenticateFirebaseToken, managerRoleCheck, updateStaff); // 직원 정보 수정
-router.put("/enable/:id", authenticateFirebaseToken, managerRoleCheck, enableUser); // 계정 활성화
-router.put("/disabled/:id", authenticateFirebaseToken, patientRoleCheck, disabledUser); // 계정 비활성화
+router.put("/:id", auth, patientRoleCheck, updateUser); // 홈페이지 회원 정보 수정
+router.put("/patient/:id", auth, staffRoleCheck, updatePatient); // 환자 정보 수정
+router.put("/staff/:id", auth, managerRoleCheck, updateStaff); // 직원 정보 수정
+router.put("/enable/:id", auth, managerRoleCheck, enableUser); // 계정 활성화
+router.put("/disabled/:id", auth, patientRoleCheck, disabledUser); // 계정 비활성화
 
-router.delete("/:id", authenticateFirebaseToken, adminRoleCheck, deleteUser); // 계정 삭제
+router.delete("/:id", auth, adminRoleCheck, deleteUser); // 계정 삭제
 
 export default router;
